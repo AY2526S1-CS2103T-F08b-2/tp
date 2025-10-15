758c1a36dcd6ad5b27339453fdeb7b86af34f4a9
Rishi <128126033+Rihiz@users.noreply.github.com>
2025-10-15 10:34:00 +0800

    Fix skill bug

--- src/main/java/seedu/address/model/skill/Skill.java
+++ src/main/java/seedu/address/model/skill/Skill.java
@@ -10,8 +10,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Skill {
 
     public static final String MESSAGE_CONSTRAINTS = "Skills names should be lowercase alphanumeric "
-            + "and may include '+' or '#' symbols, but cannot start with '#' and must be at least 2 characters long";
-    public static final String VALIDATION_REGEX = "[a-z0-9][a-z0-9+#]+";
+            + "and may include '+' or '#' symbols, but cannot start with '#' and must be at least 1 character long";
+    public static final String VALIDATION_REGEX = "[a-z0-9+#]+";
 
     public final String skillName;
     public final ExperienceLevel experienceLevel;
