792a48d922338fff5572d57fe720b37738fd8b9f
KwongWaiKin <waikin901@gmail.com>
2025-10-30 23:27:32 +0800

    Fix tests

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_GITHUB;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSON;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TELEGRAM;
 
 import java.util.logging.Logger;
@@ -33,22 +34,27 @@ public class EditCommandParser implements Parser<EditCommand> {
         logger.info("Raw arguments: " + args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args,
-                        PREFIX_NAME, PREFIX_EMAIL,
+                        PREFIX_PERSON, PREFIX_NAME, PREFIX_EMAIL,
                         PREFIX_TELEGRAM, PREFIX_GITHUB);
+
+        if (!argMultimap.getValue(PREFIX_PERSON).isPresent() || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(
+                PREFIX_PERSON, PREFIX_NAME, PREFIX_EMAIL,
+                PREFIX_TELEGRAM, PREFIX_GITHUB);
 
         Index index;
 
         try {
-            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            index = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_PERSON).get());
             logger.fine("Successfully parsed index: " + index.getOneBased());
         } catch (ParseException pe) {
-            logger.warning("Failed to parse index from preamble: " + argMultimap.getPreamble());
+            logger.warning("Failed to parse index from PREFIX_PERSON: " + argMultimap.getValue(PREFIX_PERSON).get());
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(
-                PREFIX_NAME, PREFIX_EMAIL,
-                PREFIX_TELEGRAM, PREFIX_GITHUB);
 
         EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
 
